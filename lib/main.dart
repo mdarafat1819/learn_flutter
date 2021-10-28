e678c14a96165120480f0abba617b894edf5379f
Yeasin Arafat <mdarafat1819@gmail.com>
2021-10-28 22:53:37 +0600

    Floating action button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 
 int main() {
-
-  runApp(
-    MaterialApp(
+  runApp(MaterialApp(
       title: "Flutter App",
       home: Scaffold(
         backgroundColor: Colors.green,
-        appBar: AppBar(
-          title: Text("App Bar"),
-          centerTitle: true
-        ),
+        appBar: AppBar(title: Text("App Bar"), centerTitle: true),
         body: Center(
           child: Text("Centered Text1"),
-        )
-      )
-    )
-  );
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text("+"),
+          onPressed: () {
+            print("Hello");
+          },
+        ),
+      )));
   return 0;
 }
