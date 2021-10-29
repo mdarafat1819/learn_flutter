dfe42d10bcb9ac442e18bfe68dd80e325cf21bf6
Yeasin Arafat <mdarafat1819@gmail.com>
2021-10-29 11:22:38 +0600

    HomePage class added(External)

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,40 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 int main() {
-  runApp(MaterialApp(
-      title: "Flutter App",
-      home: Scaffold(
-        backgroundColor: Colors.green,
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
-          title: Text("App Bar"), centerTitle: true
-          ),
-        body: Center(
-          child: Text("Centered Text1",
-           style: TextStyle(
-             backgroundColor: Colors.white,
-             color: Colors.blue,
-             fontSize: 30,
-             fontStyle: FontStyle.italic,
-             fontWeight: FontWeight.bold,
-           )
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          hoverColor: Colors.green,
-          backgroundColor:Colors.amber,
-
-          child: Text("+"),
-          onPressed: () {
-            print("Hello");
-          },
-        ),
-      )));
+  
+  runApp(MaterialApp(title: "Flutter App", home: HomePage()));
   return 0;
+}
+
+class HomePage extends StatelessWidget {
+  @override
+   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: Text("App Bar"),
+          centerTitle: true),
+      body: Center(
+        child: Text("Centered Text1",
+            style: TextStyle(
+              backgroundColor: Colors.white,
+              color: Colors.blue,
+              fontSize: 30,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+            )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        hoverColor: Colors.green,
+        backgroundColor: Colors.amber,
+        child: Text("+"),
+        onPressed: () {
+          print("Hello");
+        },
+      ),
+    );
+  }
 }
