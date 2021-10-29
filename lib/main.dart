dba662af0eea1a58e70a38b1410f2a91ae3ae6cc
Yeasin Arafat <mdarafat1819@gmail.com>
2021-10-29 21:08:23 +0600

    Icon added

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,14 @@ class HomePage extends StatelessWidget {
           backgroundColor: Colors.blueGrey,
           title: Text("App Bar"),
           centerTitle: true),
-      body: Center(
-        child: Image.asset("assets/images/flutter.jpeg", height: 250, width: 250),
+          body: Center(
+        //child: Image.asset("assets/images/flutter.jpeg", height: 250, width: 250),
+          child: Icon(Icons.alarm, size: 100),
       ),
       floatingActionButton: FloatingActionButton(
         hoverColor: Colors.green,
         backgroundColor: Colors.amber,
-        child: Text("+"),
+        child: Icon(Icons.message),
         onPressed: () {
           print("Hello");
         },
