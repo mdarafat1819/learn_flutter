f4c70be8f521438f95dc2154a40ae1750b9c4bc5
Yeasin Arafat <mdarafat1819@gmail.com>
2021-10-29 20:43:21 +0600

    Assets Image added

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,13 @@ import 'dart:js';
 import 'package:flutter/material.dart';
 
 int main() {
-  
   runApp(MaterialApp(title: "Flutter App", home: HomePage()));
   return 0;
 }
 
 class HomePage extends StatelessWidget {
   @override
-   Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
       appBar: AppBar(
@@ -18,7 +17,7 @@ class HomePage extends StatelessWidget {
           title: Text("App Bar"),
           centerTitle: true),
       body: Center(
-        child: Image.network("https://miro.medium.com/max/2000/1*PCKC8Ufml-wvb9Vjj3aaWw.jpeg", height: 250, width: 250,),
+        child: Image.asset("assets/images/flutter.jpeg", height: 250, width: 250),
       ),
       floatingActionButton: FloatingActionButton(
         hoverColor: Colors.green,
