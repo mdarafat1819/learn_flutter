c97a8aec7fc42ea5207f8241915852975056ecf6
Yeasin Arafat <mdarafat1819@gmail.com>
2021-11-01 00:16:04 +0600

    BottomNavigation Bar added

--- lib/main.dart
+++ lib/main.dart
@@ -16,23 +16,21 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("App Bar"), centerTitle: true),
       drawer: Drawer(
-        child: Column(
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text("Yeasin Arafat"), 
-              accountEmail: Text("mdarafat1819@gmail.com"),
-              ),
-            ListTile(
-              title: Text("Second Page"),
-              leading: Icon(Icons.home),
-              onTap:(){
-                Navigator.pop(context);
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondPage()));
-              },
-            )
-          ]
-          )
-      ),
+          child: Column(children: [
+        UserAccountsDrawerHeader(
+          accountName: Text("Yeasin Arafat"),
+          accountEmail: Text("mdarafat1819@gmail.com"),
+        ),
+        ListTile(
+          title: Text("Second Page"),
+          leading: Icon(Icons.home),
+          onTap: () {
+            Navigator.pop(context);
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => SecondPage()));
+          },
+        )
+      ])),
       body: Contact(),
       floatingActionButton: FloatingActionButton(
         hoverColor: Colors.green,
@@ -40,6 +38,18 @@ class HomePage extends StatelessWidget {
         child: Icon(Icons.message),
         onPressed: () {
           print("Hello");
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.contact_mail), label: "Contact"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "People")
+        ],
+        onTap: (index) {
+          print(index);
         },
       ),
     );
@@ -58,7 +68,6 @@ class _ContactState extends State<Contact> {
     return Container(
       padding: EdgeInsets.all(20),
       child: Column(children: [
-       
         TextField(
           maxLength: 30,
           decoration: InputDecoration(
