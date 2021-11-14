85e06231e73299aeeb6db30a8a3121f60ce1991c
Yeasin Arafat <mdarafat1819@gmail.com>
2021-11-15 00:15:13 +0600

    Form Validator

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'dart:js';
 
 import 'package:flutter/material.dart';
-import 'package:learn_flutter/secondScreen.dart';
-
-import 'secondScreen.dart';
 
 int main() {
   runApp(MaterialApp(title: "Flutter App", home: HomePage()));
@@ -15,43 +12,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("App Bar"), centerTitle: true),
-      drawer: Drawer(
-          child: Column(children: [
-        UserAccountsDrawerHeader(
-          accountName: Text("Yeasin Arafat"),
-          accountEmail: Text("mdarafat1819@gmail.com"),
-        ),
-        ListTile(
-          title: Text("Second Page"),
-          leading: Icon(Icons.home),
-          onTap: () {
-            Navigator.pop(context);
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => SecondPage()));
-          },
-        )
-      ])),
       body: Contact(),
-      floatingActionButton: FloatingActionButton(
-        hoverColor: Colors.green,
-        backgroundColor: Colors.amber,
-        child: Icon(Icons.message),
-        onPressed: () {
-          print("Hello");
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.contact_mail), label: "Contact"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "People")
-        ],
-        onTap: (index) {
-          print(index);
-        },
-      ),
     );
   }
 }
@@ -63,52 +24,52 @@ class Contact extends StatefulWidget {
 }
 
 class _ContactState extends State<Contact> {
-  TextEditingController userInput = TextEditingController();
-  var str = "Guest";
+  TextEditingController userName = TextEditingController();
+  TextEditingController password = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(20),
-      child: Column(children: [
-        TextField(
-          maxLength: 30,
-          decoration: InputDecoration(
-            labelText: "Name",
-            border: OutlineInputBorder(),
-            hintText: "Enter Your Full Name",
-            prefixIcon: Icon(Icons.account_circle),
+      child: Form(
+        key: _formkey,
+        child: Column(children: [
+          TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Fil up required";
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              hintText: "Enter Your User Name",
+              labelText: "User Name",
+              border: OutlineInputBorder(),
+             ),
           ),
-          onChanged: (value) {
-            setState(() {
-              str = value;
-            });
-          },
-        ),
-        Text("Hello $str"),
-        ElevatedButton(
-          child: Text("Got to Second Page"),
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => SecondPage()));
-          },
-        ),
-        Text(userInput.text),
-        TextField(
-          controller: userInput,
-          decoration: InputDecoration(
-            labelText: "User Input",
-            border: OutlineInputBorder(),
+          TextFormField(
+            validator: (value) {
+              if (value!.length < 4) {
+                return "Minimum 4 digit is required";
+              }
+              return null;
+            },
+            controller: userName,
+            decoration: InputDecoration(
+              labelText: "Password",
+              hintText: "Enter Your Password",
+              border: OutlineInputBorder(),
+            ),
           ),
-        ),
-       
-        ElevatedButton(
-          child: Text("print user input"),
-          onPressed: (){
-            setState(() {
-              
-            });
-          },
-        )
-      ]),
+          ElevatedButton(
+            child: Text("Submit"),
+            onPressed: () {
+              if (_formkey.currentState!.validate() == true) {
+                print("Form submitted succesfully");
+              }
+            },
+          )
+        ]),
+      ),
     );
   }
 }
