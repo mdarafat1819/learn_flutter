318a4193217bd3055e72a5f9baf835c8031cf2b1
Yeasin Arafat <mdarafat1819@gmail.com>
2022-01-05 23:11:14 +0600

    Google Map added

--- lib/geo_location.dart
+++ lib/geo_location.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Location extends StatefulWidget {
   State<StatefulWidget> createState() {
@@ -15,34 +16,51 @@ class _Location extends State<Location> {
     return position;
   }
 
+  late GoogleMapController mapController;
+  LatLng _center = LatLng(40, -122.677433);//Initialize
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+  bool locationFound = false;
+  
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FlatButton(
-            onPressed: () async {
-              Position pos = await findLocation();
-              setState(() {
-                print(pos.toString());
-              });
-            },
+    return locationFound == false
+        ? Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(
-                  Icons.location_on,
-                  size: 60,
-                  color: Color(0xfff60002),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Find My Location",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                FlatButton(
+                  onPressed: () async {
+                    Position position = await findLocation();
+                    setState(() {
+                     _center = LatLng(position!.latitude, position!.longitude);
+                      locationFound = true;
+                    });
+                  },
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        size: 60,
+                        color: Color(0xfff60002),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Find My Location",
+                        style: TextStyle(
+                            fontSize: 25, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ),
-        ],
+          )
+        : GoogleMap(
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 11.0,
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps/google_maps.dart';
 
 import './geo_location.dart';
 int main() {
